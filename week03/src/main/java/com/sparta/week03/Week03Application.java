1dea53582a77d3951a9ba3b34ca1e9aaab532704
Juhyun Lee <110277186+lee0119@users.noreply.github.com>
2022-08-16 18:30:41 +0900

    Add files via upload

---
+++ week03/src/main/java/com/sparta/week03/Week03Application.java
@@ -0,0 +1,17 @@
+package com.sparta.week03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class Week03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week03Application.class, args);
+    }
+
+}
